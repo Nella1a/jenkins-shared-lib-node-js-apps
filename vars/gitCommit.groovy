87f96c8bde865aae6b4c091749c6db0a5169e2f8
Nella1a <m.allen@gmx.at>
2025-07-29 11:07:19 +0200

    Add git commit function

---
+++ vars/gitCommit.groovy
@@ -0,0 +1,16 @@
+#! /usr/bin/env groovy
+
+def all(String currentVersion, String updatedVersion, String gitRepo) {
+  echo "Commit changes to github ... "
+
+  withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+
+  sh 'git config user.name "jenkins"'
+  sh 'git config user.email "jenkins@example.com"'
+  sh "git remote set-url origin https://${USER}:${PASS}@${gitRepo}"
+  sh 'git add .'
+  sh "git commit -m \"Updated image version from ${currentVersion} to ${updatedVersion}\""
+  sh 'git push origin HEAD:main'
+  }
+
+}
