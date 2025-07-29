e97f49281e89b3b8967b87377de561aa49b3a4c1
Martha <45481873+Nella1a@users.noreply.github.com>
2025-07-29 19:10:42 +0200

    Update dockerLogin.groovy - cleanup

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,7 +2,7 @@
 
 def call() {
   echo "Docker login ..."
-  withCredentials([usernamePassword(credentialsId: 'cred-docker', passwordVariable: 'PASS', usernameVariabel: 'USER')]){
+  withCredentials([usernamePassword(credentialsId: 'cred-docker', passwordVariable: 'PASS', usernameVariable: 'USER')]){
     sh 'echo "$PASS" | docker login -u "$USER" --password-stdin'
   }
 }
