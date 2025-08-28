6b3c7740fe76f896c758df5cccf95a7211184de8
Nella1a <m.allen@gmx.at>
2025-08-28 11:59:58 +0200

    wrap remote docker command in quotes to prevent SSH parsing issues

--- vars/deployToAWSEC2.groovy
+++ vars/deployToAWSEC2.groovy
@@ -2,9 +2,11 @@
 
 def call(String imgName, String port, String hostIp, String userName) {
   echo "Deploying to AWS EC2 Instance ..."
+
   def dockerCmd = "docker run -d -p ${port}:${port} ${imgName}"
-  sshagent(['ec2-server-key']){
-    sh "ssh -o StrictHostKeyChecking=no ${userName}@${hostIp} ${dockerCmd}"
+
+  sshagent(['ec2-server-key']) {
+    sh "ssh -o StrictHostKeyChecking=no ${userName}@${hostIp} '${dockerCmd}'"
   }
 
 }
