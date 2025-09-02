15a6e2f32eb4d1f2455fed5fbf5c8a9005205c56
Martha <45481873+Nella1a@users.noreply.github.com>
2025-09-02 13:34:09 +0200

    Update deployToAWSEC2.groovy - remove ec2 keys

--- vars/deployToAWSEC2.groovy
+++ vars/deployToAWSEC2.groovy
@@ -3,9 +3,7 @@
 def call(String IMAGE_TAG, String ec2Instance, String scriptName) {
   echo "Deploying to AWS EC2 Instance ..."
 
-  sshagent(['ec2-server-key']) {
     sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} chmod +x /home/ec2-user/${scriptName}"
     sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} bash ./${scriptName} ${IMAGE_TAG}"
-  }
-
+  
 }
